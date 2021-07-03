1334780e23f29234698a0fca26a4a1222587d17d
patrick <pkulach2@illinois.edu>
2021-07-03 13:38:24 -0500

    removed tabs and added a line for opencl parsing

--- src/main.cpp
+++ src/main.cpp
@@ -27,140 +27,118 @@ Buffer cl_output;
 Buffer cl_input;
 
 void pickPlatform(Platform& platform, const std::vector<Platform>& platforms){
-	
-	if (platforms.size() == 1) platform = platforms[0];
-	else{
-		int input = 0;
-		cout << "\nChoose an OpenCL platform: ";
-		cin >> input;
+    
+    if (platforms.size() == 1) platform = platforms[0];
+    else{
+        int input = 0;
+        cout << "\nChoose an OpenCL platform: ";
+        cin >> input;
 
-		// handle incorrect user input
-		while (input < 1 || input > platforms.size()){
-			cin.clear(); //clear errors/bad flags on cin
-			cin.ignore(cin.rdbuf()->in_avail(), '\n'); // ignores exact number of chars in cin buffer
-			cout << "No such option. Choose an OpenCL platform: ";
-			cin >> input;
-		}
-		platform = platforms[input - 1];
-	}
+        // handle incorrect user input
+        while (input < 1 || input > platforms.size()){
+            cin.clear(); //clear errors/bad flags on cin
+            cin.ignore(cin.rdbuf()->in_avail(), '\n'); // ignores exact number of chars in cin buffer
+            cout << "No such option. Choose an OpenCL platform: ";
+            cin >> input;
+        }
+        platform = platforms[input - 1];
+    }
 }
 
 void pickDevice(Device& device, const std::vector<Device>& devices){
-	
-	if (devices.size() == 1) device = devices[0];
-	else{
-		int input = 0;
-		cout << "\nChoose an OpenCL device: ";
-		cin >> input;
+    
+    if (devices.size() == 1) device = devices[0];
+    else{
+        int input = 0;
+        cout << "\nChoose an OpenCL device: ";
+        cin >> input;
 
-		// handle incorrect user input
-		while (input < 1 || input > devices.size()){
-			cin.clear(); //clear errors/bad flags on cin
-			cin.ignore(cin.rdbuf()->in_avail(), '\n'); // ignores exact number of chars in cin buffer
-			cout << "No such option. Choose an OpenCL device: ";
-			cin >> input;
-		}
-		device = devices[input - 1];
-	}
+        // handle incorrect user input
+        while (input < 1 || input > devices.size()){
+            cin.clear(); //clear errors/bad flags on cin
+            cin.ignore(cin.rdbuf()->in_avail(), '\n'); // ignores exact number of chars in cin buffer
+            cout << "No such option. Choose an OpenCL device: ";
+            cin >> input;
+        }
+        device = devices[input - 1];
+    }
 }
 
 void printErrorLog(const Program& program, const Device& device){
-	
-	// Get the error log and print to console
-	string buildlog = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device);
-	cerr << "Build log:" << std::endl << buildlog << std::endl;
+    
+    // Get the error log and print to console
+    string buildlog = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device);
+    cerr << "Build log:" << std::endl << buildlog << std::endl;
 
-	// Print the error log to a file
-	FILE *log = fopen("errorlog.txt", "w");
-	fprintf(log, "%s\n", buildlog.c_str());
-	cout << "Error log saved in 'errorlog.txt'" << endl;
-	system("PAUSE");
-	exit(1);
-}
-
-void selectRenderMode(unsigned int& rendermode){
-	cout << endl << "Rendermodes: " << endl << endl;
-	cout << "\t(1) Simple gradient" << endl;
-	cout << "\t(2) Sphere with plain colour" << endl;
-	cout << "\t(3) Sphere with cosine weighted colour" << endl;
-	cout << "\t(4) Stripey sphere" << endl;
-	cout << "\t(5) Sphere with screen door effect" << endl;
-	cout << "\t(6) Sphere with normals" << endl;
-
-	unsigned int input;
-	cout << endl << "Select rendermode (1-6): ";
-	cin >> input; 
-
-	// handle incorrect user input
-	while (input < 1 || input > 6){
-		cin.clear(); //clear errors/bad flags on cin
-		cin.ignore(cin.rdbuf()->in_avail(), '\n'); // ignores exact number of chars in cin buffer
-		cout << "No such option. Select rendermode: ";
-		cin >> input;
-	}
-	rendermode = input;
+    // Print the error log to a file
+    FILE *log = fopen("errorlog.txt", "w");
+    fprintf(log, "%s\n", buildlog.c_str());
+    cout << "Error log saved in 'errorlog.txt'" << endl;
+    system("PAUSE");
+    exit(1);
 }
 
 void initOpenCL()
 {
-	// Get all available OpenCL platforms (e.g. AMD OpenCL, Nvidia CUDA, Intel OpenCL)
-	std::vector<Platform> platforms;
-	Platform::get(&platforms);
-	cout << "Available OpenCL platforms : " << endl << endl;
-	for (int i = 0; i < platforms.size(); i++)
-		cout << "\t" << i + 1 << ": " << platforms[i].getInfo<CL_PLATFORM_NAME>() << endl;
+    // Get all available OpenCL platforms (e.g. AMD OpenCL, Nvidia CUDA, Intel OpenCL)
+    std::vector<Platform> platforms;
+    Platform::get(&platforms);
+    cout << "Available OpenCL platforms : " << endl << endl;
+    for (int i = 0; i < platforms.size(); i++)
+        cout << "\t" << i + 1 << ": " << platforms[i].getInfo<CL_PLATFORM_NAME>() << endl;
 
-	// Pick one platform
-	Platform platform;
-	pickPlatform(platform, platforms);
-	cout << "\nUsing OpenCL platform: \t" << platform.getInfo<CL_PLATFORM_NAME>() << endl;
+    // Pick one platform
+    Platform platform;
+    pickPlatform(platform, platforms);
+    cout << "\nUsing OpenCL platform: \t" << platform.getInfo<CL_PLATFORM_NAME>() << endl;
 
-	// Get available OpenCL devices on platform
-	std::vector<Device> devices;
-	platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+    // Get available OpenCL devices on platform
+    std::vector<Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
 
-	cout << "Available OpenCL devices on this platform: " << endl << endl;
-	for (int i = 0; i < devices.size(); i++){
-		cout << "\t" << i + 1 << ": " << devices[i].getInfo<CL_DEVICE_NAME>() << endl;
-		cout << "\t\tMax compute units: " << devices[i].getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << endl;
-		cout << "\t\tMax work group size: " << devices[i].getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>() << endl << endl;
-	}
+    cout << "Available OpenCL devices on this platform: " << endl << endl;
+    for (int i = 0; i < devices.size(); i++){
+        cout << "\t" << i + 1 << ": " << devices[i].getInfo<CL_DEVICE_NAME>() << endl;
+        cout << "\t\tMax compute units: " << devices[i].getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << endl;
+        cout << "\t\tMax work group size: " << devices[i].getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>() << endl << endl;
+    }
 
-	// Pick one device
-	Device device;
-	pickDevice(device, devices);
-	cout << "\nUsing OpenCL device: \t" << device.getInfo<CL_DEVICE_NAME>() << endl;
-	cout << "\t\t\tMax compute units: " << device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << endl;
-	cout << "\t\t\tMax work group size: " << device.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>() << endl;
+    // Pick one device
+    Device device;
+    pickDevice(device, devices);
+    cout << "\nUsing OpenCL device: \t" << device.getInfo<CL_DEVICE_NAME>() << endl;
+    cout << "\t\t\tMax compute units: " << device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << endl;
+    cout << "\t\t\tMax work group size: " << device.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>() << endl;
 
-	// Create an OpenCL context and command queue on that device.
-	context = Context(device);
-	queue = CommandQueue(context, device);
+    // Create an OpenCL context and command queue on that device.
+    context = Context(device);
+    queue = CommandQueue(context, device);
 
-	// Convert the OpenCL source code to a string
-	string source;
-	ifstream file("src/opencl_kernel.cl");
-	if (!file){
-		cout << "\nNo OpenCL file found!" << endl << "Exiting..." << endl;
-		system("PAUSE");
-		exit(1);
-	}
-	while (!file.eof()){
-		char line[256];
-		file.getline(line, 255);
-		source += line;
-	}
+    // Convert the OpenCL source code to a string
+    string source;
+    ifstream file("src/opencl_kernel.cl");
+    if (!file){
+        cout << "\nNo OpenCL file found!" << endl << "Exiting..." << endl;
+        system("PAUSE");
+        exit(1);
+    }
+    while (!file.eof()){
+        char line[256];
+        file.getline(line, 255);
+        source += line;
+		source += "\n";
+    }
 
-	const char* kernel_source = source.c_str();
+    const char* kernel_source = source.c_str();
 
-	// Create an OpenCL program by performing runtime source compilation for the chosen device
-	program = Program(context, kernel_source);
-	cl_int result = program.build({ device });
-	if (result) cout << "Error during compilation OpenCL code!!!\n (" << result << ")" << endl;
-	if (result == CL_BUILD_PROGRAM_FAILURE) printErrorLog(program, device);
+    // Create an OpenCL program by performing runtime source compilation for the chosen device
+    program = Program(context, kernel_source);
+    cl_int result = program.build({ device });
+    if (result) cout << "Error during compilation OpenCL code!!!\n (" << result << ")" << endl;
+    if (result == CL_BUILD_PROGRAM_FAILURE) printErrorLog(program, device);
 
-	// Create a kernel (entry point in the OpenCL source program)
-	kernel = Kernel(program, "render_kernel");
+    // Create a kernel (entry point in the OpenCL source program)
+    kernel = Kernel(program, "render_kernel");
 }
 
 inline float clamp(float x){ return x < 0.0f ? 0.0f : x > 1.0f ? 1.0f : x; }
@@ -174,6 +152,7 @@ int main(int argc, char **argv) {
     SDL_Init(SDL_INIT_VIDEO);
 
     // Create an SDL window
+    SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
     SDL_Window *window = SDL_CreateWindow("R-Two", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
 
     // Create a renderer (accelerated and in sync with the display refresh rate)
@@ -187,35 +166,35 @@ int main(int argc, char **argv) {
     // Initial renderer color
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
-	//cpu_output = new cl_float3[WINDOW_WIDTH * WINDOW_HEIGHT];
-	
+    //cpu_output = new cl_float3[WINDOW_WIDTH * WINDOW_HEIGHT];
+    
 
-	initOpenCL();
+    initOpenCL();
 
-	// Create image buffer on the OpenCL device
-	cl_output = Buffer(context, CL_MEM_WRITE_ONLY, WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(Uint32));
-	cl_input  = Buffer(context, CL_MEM_READ_ONLY,  WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(Uint32));
+    // Create image buffer on the OpenCL device
+    cl_output = Buffer(context, CL_MEM_WRITE_ONLY, WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(Uint32));
+    cl_input  = Buffer(context, CL_MEM_READ_ONLY,  WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(Uint32));
 
-	// pick a rendermode
-	unsigned int rendermode = 1;
-	unsigned long frameCount = 0;
-	//selectRenderMode(rendermode);
+    // pick a rendermode
+    unsigned int rendermode = 1;
+    unsigned long frameCount = 0;
+    //selectRenderMode(rendermode);
 
-	// specify OpenCL kernel arguments
-	kernel.setArg(0, cl_output);
-	kernel.setArg(1, cl_input);
-	kernel.setArg(2, WINDOW_WIDTH);
-	kernel.setArg(3, WINDOW_HEIGHT);
-	kernel.setArg(4, frameCount);
-	kernel.setArg(5, rendermode);
+    // specify OpenCL kernel arguments
+    kernel.setArg(0, cl_output);
+    kernel.setArg(1, cl_input);
+    kernel.setArg(2, WINDOW_WIDTH);
+    kernel.setArg(3, WINDOW_HEIGHT);
+    kernel.setArg(4, frameCount);
+    kernel.setArg(5, rendermode);
 
-	std::size_t global_work_size = WINDOW_WIDTH * WINDOW_HEIGHT;
-	std::size_t local_work_size = 64; 
+    std::size_t global_work_size = WINDOW_WIDTH * WINDOW_HEIGHT;
+    std::size_t local_work_size = 64; 
 
     bool running = true;
     SDL_Event event;
     while(running) {
-	    // Process events
+        // Process events
         while(SDL_PollEvent(&event)) {
             if(event.type == SDL_QUIT) {
                 running = false;
@@ -224,29 +203,29 @@ int main(int argc, char **argv) {
                 if(strcmp(key, "Escape") == 0) {
                     running = false;
                 }                    
-				if(strcmp(key, "Space") == 0) {
+                if(strcmp(key, "Space") == 0) {
                     pixelBuffer.clear();
-					frameCount = 0;
+                    frameCount = 0;
                 }      
             }
         }
-		kernel.setArg(4, ++frameCount);
-		
-		// Copy previous frame to GPU
-		queue.enqueueWriteBuffer(cl_input, CL_TRUE, 0, WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(Uint32), pixelBuffer.pixels);
+        kernel.setArg(4, ++frameCount);
+        
+        // Copy previous frame to GPU
+        queue.enqueueWriteBuffer(cl_input, CL_TRUE, 0, WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(Uint32), pixelBuffer.pixels);
 
-		// Clear screen
+        // Clear screen
         SDL_UpdateTexture(texture, NULL, pixelBuffer.pixels,  pixelBuffer.width * sizeof(Uint32));
         SDL_RenderClear(renderer);
 
 
-		// Draw
-		// launch the kernel
-		queue.enqueueNDRangeKernel(kernel, NULL, global_work_size, local_work_size);
-		queue.finish();
+        // Draw
+        // launch the kernel
+        queue.enqueueNDRangeKernel(kernel, NULL, global_work_size, local_work_size);
+        queue.finish();
 
-		// read and copy OpenCL output to pixel buffer
-		queue.enqueueReadBuffer(cl_output, CL_TRUE, 0, WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(Uint32), pixelBuffer.pixels);
+        // read and copy OpenCL output to pixel buffer
+        queue.enqueueReadBuffer(cl_output, CL_TRUE, 0, WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(Uint32), pixelBuffer.pixels);
 
         SDL_RenderCopy(renderer, texture, NULL, NULL);
 
--- src/opencl_kernel.cl
+++ src/opencl_kernel.cl
@@ -1,41 +1,41 @@
 float noise(float3 seed) {
-	float floor;
+    float floor;
     return fract(sin(dot(seed, (float3)(1323232.9898f,7843432.233f, 23872.23232f)) * 4375348.5453123f), &floor);
 }
 
 uint float4ToInt32(float4 color) {
-	uint r = (uint)(color.x * 255.99999f);
-	uint g = (uint)(color.y * 255.99999f);
-	uint b = (uint)(color.z * 255.99999f);
-	uint a = (uint)(color.w * 255.99999f);
-	uint conversion = (r << 24) | (g << 16) | (b << 8) | a;
-	return conversion;
+    uint r = (uint)(color.x * 255.99999f);
+    uint g = (uint)(color.y * 255.99999f);
+    uint b = (uint)(color.z * 255.99999f);
+    uint a = (uint)(color.w * 255.99999f);
+    uint conversion = (r << 24) | (g << 16) | (b << 8) | a;
+    return conversion;
 }
 
 float4 int32ToFloat4(uint color) {
-	uint r = (color & 0xF000 ) >> 24;
-	uint g = (color & 0x0F00 ) >> 16;
-	uint b = (color & 0x00F0 ) >> 8;
-	uint a = (color & 0x000F );
-	return (float4)((float)r, (float)g, (float)b, (float)a) / 255.0f;
+    uint r = (color & 0xFF000000 ) >> 24;
+    uint g = (color & 0x00FF0000 ) >> 16;
+    uint b = (color & 0x0000FF00 ) >> 8;
+    uint a = (color & 0x000000FF );
+    return (float4)((float)r, (float)g, (float)b, (float)a) / 255.0f;
 }
 
 __kernel void render_kernel(__global uint *output, __global uint *input, int width, int height, unsigned long frameCount, int rendermode)
 {
-	const int work_item_id = get_global_id(0);		/* the unique global id of the work item for the current pixel */
-	int x_coord = work_item_id % width;					/* x-coordinate of the pixel */
-	int y_coord = work_item_id / width;					/* y-coordinate of the pixel */
+    const int work_item_id = get_global_id(0);        /* the unique global id of the work item for the current pixel */
+    int x_coord = work_item_id % width;                    /* x-coordinate of the pixel */
+    int y_coord = work_item_id / width;                    /* y-coordinate of the pixel */
 
-	float2 uv = (float2)((float)x_coord / (float)width, (float)y_coord / (float)height);
+    float2 uv = (float2)((float)x_coord / (float)width, (float)y_coord / (float)height);
 
-	/* generate new noise each frame */
-	float n = noise((float3)(uv, frameCount));
-	float4 result = (float4)(n, n, n, 1.0f);
-	
-	/* blend */
-	/*result = (int32ToFloat4(input[work_item_id]) * (frameCount-1)  + result) / (frameCount);
-	*/
-	
-	/* convert to RGBA8888 */
-	output[work_item_id] = float4ToInt32(result);
+    /* generate new noise each frame */
+    float n = noise((float3)(uv, frameCount));
+    float4 result = (float4)(n, n, n, 1.0f);
+    
+    /* blend */
+    /*result = (int32ToFloat4(input[work_item_id]) * (frameCount-1)  + result) / (frameCount);
+    */
+    
+    /* convert to RGBA8888 */
+    output[work_item_id] = float4ToInt32(result);
 }
